e8103a8cce2efc0970aa9a4b34c2dee92d472322
Student <student@Makerss-MacBook-Air.local>
2020-05-12 17:56:39 +0100

    started writed spec test for result of release_bike method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,5 +6,10 @@ describe DockingStation do
       expect(DockingStation.new).to respond_to(:release_bike)
     end
     it { is_expected.to respond_to(:release_bike) }
+
+    it "should release a bike when release_bike is called" do
+       expect(subject.release_bike).to eq(bike)
+    end
+
   end
 end
