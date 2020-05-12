88eadeca1e053f587e60bc319298ba6ee4f46246
Student <student@Makerss-MacBook-Air.local>
2020-05-12 17:03:16 +0100

    passed spec test for release_bike method with one liner syntax

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,5 +5,6 @@ describe DockingStation do
     it "should release a bike from the docking station" do
       expect(DockingStation.new).to respond_to(:release_bike)
     end
+    it { is_expected.to respond_to(:release_bike) }
   end
 end
