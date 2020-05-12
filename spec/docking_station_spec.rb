d6306ee8aa1627d99dd39969bde8d862d9978290
Student <student@Makerss-MacBook-Air.local>
2020-05-12 16:47:13 +0100

    wrote release_bike method and passed rspec

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,9 @@
 require 'docking_station'
 
 describe DockingStation do
-  it 'exists' do
+  describe '#release_bike' do 
+    it "should release a bike from the docking station" do
+      expect(DockingStation.new).to respond_to(:release_bike)
+    end
   end
 end
